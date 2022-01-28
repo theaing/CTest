a75d0a4da5bd1a7f1e92839aaa905ad3898a84d8
“theaking” <443896460@qq.com>
2022-01-28 23:41:00 +0800

    edit libevent

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:main.c
-	@gcc main.c -o test
+	@gcc main.c -o test -I/usr/local/Cellar/libevent/2.1.12/include -L/usr/local/Cellar/libevent/2.1.12/lib -levent
 	@./test
 	@rm -rf test
--- main.c
+++ main.c
@@ -1,8 +1,15 @@
-#include<stdlib.h>
-#include <stdio.h>
-#include<string.h>
+#include <stdlib.h>
+#include <event.h>
+
 int main(int argc, char const *argv[])
 {
-    printf("hello world\n");
+    const char ** methods = event_get_supported_methods();
+    int i = 0;
+    for ( i = 0; methods[i] !=NULL; i++)
+    {
+       printf("%s\n",methods[i]);
+    }
+    struct event_base * base = event_base_new();
+    printf("%s\n",event_base_get_method(base));
     return 0;
 }
