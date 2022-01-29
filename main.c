814e6573463c04774ebfd4fe4e9c23708c0423d0
“theaking” <443896460@qq.com>
2022-01-29 15:12:51 +0800

    tst

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
 #include <stdlib.h>
 #include <event.h>
-
 int main(int argc, char const *argv[])
 {
-    const char ** methods = event_get_supported_methods();
-    int i = 0;
-    for ( i = 0; methods[i] !=NULL; i++)
-    {
-       printf("%s\n",methods[i]);
-    }
-    struct event_base * base = event_base_new();
-    printf("%s\n",event_base_get_method(base));
+    // const char ** methods = event_get_supported_methods();
+    // int i = 0;
+    // for ( i = 0; methods[i] !=NULL; i++)
+    // {
+    //    printf("%s\n",methods[i]);
+    // }
+    // struct event_base * base = event_base_new();
+    // printf("%s\n",event_base_get_method(base));
     return 0;
 }
